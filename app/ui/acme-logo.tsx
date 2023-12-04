480665a35271dddf15751a87ea3f59448cf86bb6
Sergiu Plesco <plescosergiu@gmail.com>
2023-12-04 21:07:18 +0200

    added authentication with next-auth with credentials provider

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,14 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 export default function AcmeLogo() {
   return (
-    <div className={` flex flex-row items-center leading-none text-white`}>
+    <Link
+      href={'/'}
+      className={`flex cursor-pointer flex-row items-center leading-none text-white`}
+    >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
-    </div>
+    </Link>
   );
 }
