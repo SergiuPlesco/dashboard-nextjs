0390a96f1f91ecf5107af022149e6538a9ed4f09
Sergiu Plesco <plescosergiu@gmail.com>
2023-12-06 20:16:13 +0200

    added metadata - page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import '@/app/ui/global.css';
 import { lusitana } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Acme Dashboard.',
+};
 export default function RootLayout({
   children,
 }: {
