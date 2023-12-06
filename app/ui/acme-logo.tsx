f190abc5315d40cf8ac3cab5deed5c60b987c7da
Sergiu Plesco <plescosergiu@gmail.com>
2023-12-06 19:45:57 +0200

    fixed error Link as child of Link in AcemLogo component

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,14 +1,12 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
 
 export default function AcmeLogo() {
   return (
-    <Link
-      href={'/'}
+    <div
       className={`flex cursor-pointer flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
-    </Link>
+    </div>
   );
 }
